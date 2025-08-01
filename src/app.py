f4db8ace5fcfa050a4722904a5bcffcc2541e370
say0ns <simanpito@gmail.com>
2025-08-01 10:01:06 +0000

    Web app using flask[wip]

--- src/app.py
+++ src/app.py
@@ -7,33 +7,27 @@ with open('../src/modelos-random-forest.pkl', 'rb') as f:
 
 app = Flask(__name__)
 
-
 model = modelos['rf_classifier_model']
-class_dict = {"0": "No Diabetes",
-              "1": "Diabetes"
-              }
-
+class_dict = {
+    0: "No Diabetes",
+    1: "Diabetes"
+}
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
         return render_template('index.html', prediction=None)
     if request.method == "POST":
-        val1 = float(request.form["val1"])
-        val2 = float(request.form["val2"])
-        val3 = float(request.form["val3"])
-        val4 = float(request.form["val4"])
-        val5 = float(request.form["val5"])
-        val6 = float(request.form["val6"])
-        val7 = float(request.form["val7"])
-        val8 = float(request.form["val8"])
-        data = [[val1, val2, val3, val4, val5, val6, val7, val8]]
-        prediction = str(model.predict(data)[0])
-        pred_class = class_dict[prediction]
-        return render_template('index.html', prediction=pred_class)
+        try:
+            data = [
+                [float(request.form[f"val{i}"]) for i in range(1, 9)]
+            ]
+            prediction = int(model.predict(data)[0])
+            pred_class = class_dict[prediction]
+            return render_template('index.html', prediction=pred_class)
+        except Exception as e:
+            return f"Error: {e}", 500
     return None
 
-
 if __name__ == '__main__':
-    # PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=3000, debug=True)
